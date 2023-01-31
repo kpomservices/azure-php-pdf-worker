c62f8909faf06dd634d6437bb8abfd44b5749d40
kpomservices <kpomservices@gmail.com>
2023-01-31 19:42:51 +0530

    updated for test decode encode

--- HttpTrigger/index.php
+++ HttpTrigger/index.php
@@ -14,7 +14,9 @@
             
             //$cwidth = 750;
             //$cheight = 600;            
-            $jsonData = rawurldecode($query['jsonData']);
+            $jsonData = preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",$query['jsonData']); 
+            $jsonData = html_entity_decode($jsonData,null,'UTF-8');
+            // $jsonData = urldecode($query['jsonData']);
             $message = $jsonData;
             // $cwidth = $query['cwidth'];
             // $cheight = $query["cheight"];
