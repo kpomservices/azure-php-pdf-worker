64e5fc047a17022dbdf0de77ad9e8be3ee8de205
kpomservices <kpomservices@gmail.com>
2023-03-09 20:22:44 +0530

    updated for fit to page

--- HttpTrigger/index.php
+++ HttpTrigger/index.php
@@ -396,7 +396,7 @@
             // fclose($fh);
             // unlink($filetoUpload);
             
-            $message = $img;
+            $message = "Test" . $img;
             //$message = $pdf->Output('svgtopdf.pdf', "E");    // send the file in
         } else {
             $contentType = "text/plain";
