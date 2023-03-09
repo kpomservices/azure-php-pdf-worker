c679930cc72c184287c69a2a0419f51ddb972c8e
kpomservices <kpomservices@gmail.com>
2023-03-09 20:38:19 +0530

    updated for fit to page

--- HttpTrigger/index.php
+++ HttpTrigger/index.php
@@ -305,9 +305,9 @@
 
                     $pdf->SetXY(0, 0);
         
-                    $img = file_get_contents('https://papdfgen.blob.core.windows.net/objectimages/640886be6d6f2.png');
-                    $pdf->Image('@' . $img);
-                    // $pdf->Image('https://papdfgen.blob.core.windows.net/objectimages/640886be6d6f2.png', 150, 10, 100, 100, '', '', 'T', false, 300, '', false, false, 0, false, false, false);
+                    // $img = file_get_contents('https://papdfgen.blob.core.windows.net/objectimages/640886be6d6f2.png');
+                    // $pdf->Image('@' . $img);
+                    $pdf->Image('https://papdfgen.blob.core.windows.net/objectimages/6409f630b9b4a.jpeg', 150, 10, 100, 100, '', '', 'T', false, 300, '', false, false, 0, false, false, false);
             
                     $pdf->StartTransform();
                     // Set Clipping Mask
