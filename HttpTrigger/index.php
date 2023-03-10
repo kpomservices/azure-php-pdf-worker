cbfc88298f0647fba3016d387e8d6dd1ee717e1b
kpomservices <kpomservices@gmail.com>
2023-03-10 15:54:58 +0530

    pdf image attach

--- HttpTrigger/index.php
+++ HttpTrigger/index.php
@@ -307,7 +307,7 @@
         
                     // $img = file_get_contents('https://papdfgen.blob.core.windows.net/objectimages/640886be6d6f2.png');
                     // $pdf->Image('@' . $img);
-                    $imginfo = $pdf->Image('https://papdfgen.blob.core.windows.net/objectimages/6409f630b9b4a.jpeg', 150, 10, 100, 100, '', '', 'T', false, 300, '', false, false, 0, false, false, false);
+                    @$pdf->Image('https://papdfgen.blob.core.windows.net/objectimages/6409f630b9b4a.jpeg', 150, 10, 100, 100, '', '', 'T', false, 300, '', false, false, 0, false, false, false);
             
                     $pdf->StartTransform();
                     // Set Clipping Mask
@@ -396,7 +396,7 @@
             // fclose($fh);
             // unlink($filetoUpload);
             
-            $message = $destinationURL . $imginfo;
+            $message = $destinationURL;
             //$message = $pdf->Output('svgtopdf.pdf', "E");    // send the file in
         } else {
             $contentType = "text/plain";
